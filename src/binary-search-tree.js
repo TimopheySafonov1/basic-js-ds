1500f7654d172e633a797debdcea1d270989c862
TimopheySafonov1 <earth2002ocean@gmail.com>
2023-12-22 00:20:46 +0100

    fix: add depedences

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+//const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -138,3 +138,7 @@ class BinarySearchTree {
     return current.data;
   }
 }
+
+module.exports = {
+  BinarySearchTree
+};
